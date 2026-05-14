2aeece4511eba9a97a96bb58ecc1c49d1d6dee11
Daniel S. Nyongesa <Dsn.globaldev@gmail.com>
2026-05-14 17:48:05 +0300

    Data types and Numeric variables

---
+++ Week 2/Data_types_and_Numeric_variables.py
@@ -0,0 +1,44 @@
+#First part is deals with data types and their representation
+song1 = "213" #this is a string which is a sequence of characters.
+song2 = 213 #this is a number (int) 
+
+song3 = 213.5 #this is a number (float)
+
+#Behavior of variables 
+
+song4 = "150"
+song5 = 250.25
+
+playlist = int(song4) + (song5)
+
+print (f"The playlist is {playlist} seconds.")
+#when two strings are added together, python cancatinates them. 
+#if you use the plus sign in python, all the variable should be of the same type
+
+#converting string to numbers
+
+song1 = 162
+song2 = 175
+song3 = float(input("What is the length of the song? "))
+
+#song3_input = int(song3)
+
+playlist = song1 + song2 + song3
+print(f"The playlist has {playlist} songs")
+
+
+#Expressions and mathematical operations 
+x = 3
+y = 4
+z = 5
+
+result = x + y + z
+print (result)
+#to get a full number on division, use // sign.
+#to get the remainder/modululus, use % sign
+
+#exponent sign, use **
+#multiplication sign use *
+
+
+
